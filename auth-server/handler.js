1d2c1d151a19f9f025802447d6af0328c74c074b
Andres Rivera <89215774+Ak4ni@users.noreply.github.com>
2022-03-28 16:25:22 -0400

    configuring live server for testing

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -59,3 +59,38 @@ module.exports.getAuthURL = async () => {
     }),
   };
 };
+
+//module.export.getAccessToken = async (event) => {
+ // const oAuth2Client = new google.auth.OAuth2(
+  //  client_id,
+  //  client_secret,
+ //   redirect_uris[0]
+ // );
+//  // decode authorization code extracted from the url query
+ // const code = decodeURIComponent(`${event.pathParameters.code}`);
+//
+ // return new Promise((resolve, reject) => {
+
+  //  oAuth2Client.getToken(code, (err, token) => {
+    //  if (err) {
+     //   return reject(err);
+     // }
+     // return resolve(token);
+     // });
+   // })
+   // .then ((token) => {
+     // //repond with oauth token
+     // return{
+       // statusCode: 200,
+       // body: JSON.stringify(token),
+     // };
+   // })
+    //.cath((err) => {
+     // //Hadle error
+     // console.error(err);
+     //return {
+       // statusCode: 500,
+       // body: JSON.stringify(err),
+     // };
+   // });
+//};
